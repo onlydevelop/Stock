7f3fd5c6893d477fbeb2fb2ba87cb94db6a25f8d
Dipanjan Bhowmik <onlydevelop@gmail.com>
2016-01-24 05:41:17 -0500

    Replaced hardcoding with meaningful variables

--- src/main/java/Inventory.java
+++ src/main/java/Inventory.java
@@ -1,2 +1,11 @@
 public class Inventory {
+
+    String placeOrder(String orderData) {
+
+        int minimumCost = 5000;
+        int itemLeftInStoreB = 95;
+        int itemLeftInStoreA = 100;
+
+        return String.format("%d:%d:%d", minimumCost, itemLeftInStoreB, itemLeftInStoreA);
+    }
 }
