dd0b2bb1c3a4cc03be114cd9ff7387a555341293
Dipanjan Bhowmik <onlydevelop@gmail.com>
2016-01-24 06:41:03 -0500

    Implement the logic to pass the unit test

--- src/main/java/InputParser.java
+++ src/main/java/InputParser.java
@@ -1,7 +1,20 @@
 public class InputParser {
 
-    Order parse(String inputData) {
+    Order parse(String inputData) throws Exception {
+
+        if(inputData == null || inputData == "") {
+            throw new Exception("Input cannot be null or empty");
+        }
+
         String[] inputTokens = inputData.split(":");
-        return new Order(inputTokens[0], Integer.parseInt(inputTokens[1]));
+        int orderQuantity = 0;
+
+        try {
+            orderQuantity = Integer.parseInt(inputTokens[1]);
+        } catch (NumberFormatException nfe) {
+            throw new Exception("Input data is invalid");
+        }
+
+        return new Order(inputTokens[0], orderQuantity);
     }
 }
