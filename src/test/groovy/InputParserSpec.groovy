9721d398b131987f13323746c64ac8d2cd29b2b7
Dipanjan Bhowmik <onlydevelop@gmail.com>
2016-01-24 06:07:19 -0500

    Added few negative test cases

--- src/test/groovy/InputParserSpec.groovy
+++ src/test/groovy/InputParserSpec.groovy
@@ -14,4 +14,43 @@ class InputParserSpec extends Specification{
         order.getStoreName() == "B"
         order.getOrderQuantity() == 5
     }
+
+    def "throw exception if the input is null"() {
+
+        setup:
+        InputParser inputParser = new InputParser()
+
+        when:
+        Order order = inputParser.parse(null)
+
+        then:
+        def e = thrown(Exception)
+        e.message == "Input cannot be null or empty"
+    }
+
+    def "throw exception if the input is empty"() {
+
+        setup:
+        InputParser inputParser = new InputParser()
+
+        when:
+        Order order = inputParser.parse("")
+
+        then:
+        def e = thrown(Exception)
+        e.message == "Input cannot be null or empty"
+    }
+
+    def "throw exception if the input is invalid"() {
+
+        setup:
+        InputParser inputParser = new InputParser()
+
+        when:
+        Order order = inputParser.parse("invalid:data")
+
+        then:
+        def e = thrown(Exception)
+        e.message == "Input data is invalid"
+    }
 }
