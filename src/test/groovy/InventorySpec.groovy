3817297534c0401894501c345b15acd21464100d
Dipanjan Bhowmik <onlydevelop@gmail.com>
2016-01-24 05:40:34 -0500

    Refactored the test

--- src/test/groovy/InventorySpec.groovy
+++ src/test/groovy/InventorySpec.groovy
@@ -2,18 +2,23 @@ import spock.lang.Specification
 
 class InventorySpec extends Specification{
 
-    def "instance is created for Inventory"() {
-        setup:
-            Inventory inventory = new Inventory()
+    Inventory inventory
 
+    def setup() {
+        inventory = new Inventory()
+    }
+    
+    def "instance is created for Inventory"() {
         expect:
             inventory != null
     }
 
     def "placing 5 items from StoreB"() {
-        setup:
-            Inventory inventory = new Inventory()
         expect:
             inventory.placeOrder("B:5") == "5000:95:100"
+    }
+
+    def cleanup() {
+        inventory = null
     }
 }
