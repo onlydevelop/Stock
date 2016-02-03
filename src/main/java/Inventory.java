5fb788be31d852a88a182a6abad08b192581fb09
Dipanjan Bhowmik <onlydevelop@gmail.com>
2016-02-03 15:01:47 -0500

    Extracted Store object from class members

--- src/main/java/Inventory.java
+++ src/main/java/Inventory.java
@@ -1,25 +1,22 @@
 public class Inventory {
 
-    private static final int INITIAL_ITEM_COUNT = 100;
-    private static final int ITEM_COST_IN_STORE_A = 50;
-    private static final int ITEM_COST_IN_STORE_B = 100;
+    private final Store storeA = new Store("A", 100, 50);
+    private final Store storeB = new Store("B", 100, 100);
+
     private static final int STOCK_TRANSFER_BLOCK_SIZE = 10;
     private static final int STOCK_TRANSFER_COST_PER_BLOCK = 400;
 
-    private int itemLeftInStoreA = INITIAL_ITEM_COUNT;
-    private int itemLeftInStoreB = INITIAL_ITEM_COUNT;
-
     String placeOrder(String orderData) {
 
-        int minimumCostFromStoreA = 0;
-        int minimumCostFromStoreB = 0;
-        int minimumCost = 0;
+        float minimumCostFromStoreA = 0;
+        float minimumCostFromStoreB = 0;
+        float minimumCost = 0;
 
         try {
             Order order = new InputParser().parse(orderData);
 
-            minimumCostFromStoreA = ITEM_COST_IN_STORE_A * order.getOrderQuantity();
-            minimumCostFromStoreB = ITEM_COST_IN_STORE_B * order.getOrderQuantity();
+            minimumCostFromStoreA = storeA.getUnitCost() * order.getOrderQuantity();
+            minimumCostFromStoreB = storeB.getUnitCost() * order.getOrderQuantity();
 
             if(order.getStoreName().equals("A")) {
                 minimumCostFromStoreB += Math.ceil((float)order.getOrderQuantity() / STOCK_TRANSFER_BLOCK_SIZE) * STOCK_TRANSFER_COST_PER_BLOCK;
@@ -29,16 +26,16 @@ public class Inventory {
 
             if(minimumCostFromStoreA < minimumCostFromStoreB) {
                 minimumCost = minimumCostFromStoreA;
-                itemLeftInStoreA -= order.getOrderQuantity();
+                storeA.setItemCount(storeA.getItemCount() - order.getOrderQuantity());
             } else {
                 minimumCost = minimumCostFromStoreB;
-                itemLeftInStoreB -= order.getOrderQuantity();
+                storeB.setItemCount(storeB.getItemCount() - order.getOrderQuantity());
             }
 
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
 
-        return String.format("%d:%d:%d", minimumCost, itemLeftInStoreB, itemLeftInStoreA);
+        return String.format("%d:%d:%d", (int)minimumCost, storeB.getItemCount(), storeA.getItemCount());
     }
 }
